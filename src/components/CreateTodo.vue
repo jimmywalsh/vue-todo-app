1c394dd8ee200a3d5326b09a65af63e01a080ec5
James Walsh <james.walsh@vasion.com>
2023-01-12 13:58:23 -0700

    Save todo on button click

--- src/components/CreateTodo.vue
+++ src/components/CreateTodo.vue
@@ -1,10 +1,26 @@
 <script lang="ts" setup>
+import useTodosStore from '@/stores/todosStore'
+import { TodoItem } from '@/types';
+import { ref } from 'vue'
+
+const { addTodo } = useTodosStore()
+const currentTodoText = ref('')
+
+const handleAddTodo =() => {
+  const todo: TodoItem = {
+    text: currentTodoText.value,
+    completed: false,
+    createdAt: new Date()
+  }
+
+  addTodo(todo)
+}
 </script>
 
 <template>
   <div class="my-2">
-    <input class="rounded" type="text" placeholder="New To-do" />
-    <button type="button" class="bg-indigo-500 h-10 rounded w-24 mx-1 ">
+    <input v-model="currentTodoText" class="rounded" type="text" placeholder="New To-do" />
+    <button @click="handleAddTodo" type="button" class="bg-indigo-500 h-10 rounded w-24 mx-1 ">
       Save
     </button>
   </div>
