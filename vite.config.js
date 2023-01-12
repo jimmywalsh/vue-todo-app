3c8457d7c9f55f11aaadb22d78ec5d8490eb26de
James Walsh <james.walsh@vasion.com>
2023-01-12 11:35:35 -0700

    Setup typescript support in project

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    tsconfigPaths(),
+    vue()
+  ],
 })
